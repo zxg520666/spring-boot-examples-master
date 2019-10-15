e1c76be091d9505e2331c9f632b7f48847b8cef2
zhangxiaoguang <zhangxiaoguang>
2019-10-15 15:10:59 +0800

    第一次提交

---
+++ spring-boot-web/src/main/java/com/zxg/springboot/WebApplication.java
@@ -0,0 +1,15 @@
+package com.zxg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangxiaoguang
+ * @createtime 2019/10/15-14:39
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
